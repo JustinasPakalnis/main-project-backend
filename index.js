b232cfa1341681a68a5544f30eaeefb24927ba19
JustinasPakalnis <justinas.p@inbox.lt>
2024-10-17 21:30:22 +0300

    push

--- index.js
+++ index.js
@@ -19,7 +19,11 @@ app.use(cors());
 
 app.use((req, res, next) => {
   const now = new Date();
-  console.log(`[${now.toISOString()}] ${req.method} request to ${req.url}`);
+  console.log(
+    `[${now.split("T").join(" ").slice(0, 16)}] ${req.method} request to ${
+      req.url
+    }`
+  );
   next();
 });
 
